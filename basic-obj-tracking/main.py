ea3bc9164da092bcc59d4d14e2867bb1bbb31f91
Shounak-Ghosh <shounak.ghosh.pi@gmail.com>
2022-10-29 18:24:17 -0400

    basic obj tracking tutorial code

---
+++ basic-obj-tracking/main.py
@@ -0,0 +1,57 @@
+import cv2
+from tracker import *
+
+# Create tracker object
+tracker = EuclideanDistTracker()
+
+cap = cv2.VideoCapture("data/highway.mp4")
+
+# Object detection from Stable camera (background is not changing, only objects are moving)
+object_detector = cv2.createBackgroundSubtractorMOG2(
+    history=100, varThreshold=40)
+# history: how many frames to consider for background
+# varThreshold: threshold for the pixel-wise variance
+
+while cap.isOpened():
+    ret, frame = cap.read()
+    height, width, _ = frame.shape
+    # print(height, width)
+
+    # Extract Region of interest (area closest to the camera)
+    roi = frame[340: 720, 500: 800]
+
+    # Object detection
+    mask = object_detector.apply(roi)
+    # remove all grey pixels, only keep white
+    _, mask = cv2.threshold(mask, 254, 255, cv2.THRESH_BINARY)
+    countours, _ = cv2.findContours(
+        mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    detections = []
+
+    for cnt in countours:
+        # Calculate area and remove small elements
+        area = cv2.contourArea(cnt)
+        if area > 100:
+            #  cv2.drawContours(roi, [cnt], -1, (0, 255, 0), 2)
+            x, y, w, h = cv2.boundingRect(cnt)
+            cv2.rectangle(roi, (x, y), (x+w, y+h), (0, 255, 0), 2)
+            detections.append([x, y, w, h])
+
+    # Object Tracking
+    boxes_ids = tracker.update(detections)
+    for box_id in boxes_ids:
+        x, y, w, h, id = box_id
+        cv2.putText(roi, str(id), (x, y-15),
+                    cv2.FONT_HERSHEY_PLAIN, 2, (0, 255, 0), 2)
+        cv2.rectangle(roi, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+    # print(detections)
+    cv2.imshow("Frame", frame)
+    cv2.imshow("Mask", mask)
+
+    key = cv2.waitKey(30) # waitkey 0 to go frame by frame
+    if key == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
